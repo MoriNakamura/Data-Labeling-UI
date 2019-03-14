7319c7628eb1764d7cfcd27e0aafc8c0c932b9df
Frank Chung <MoriNakamura@users.noreply.github.com>
2019-03-14 15:29:51 +0800

    Getting random data for labeling on each load

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Markup
+
+# For dataset reading and writing
+import pandas as pd
+
+unlabeled_dataset = pd.read_csv('./dataset/all_unlabeled_data.tsv', sep='\t')
+
+
+# labeled_dataset = 
 
 app = Flask(__name__)
 
@@ -6,7 +14,17 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET', 'POST'])
 def main():
 	if request.method == 'GET':
-		return render_template('index.html')
+		data = get_random_data()
+		print(data)
+		return render_template('index.html', _id=str(data[0]), text=convert_str(data))
 	else:
 		#TODO receive data
 		return ender_template('index.html')
+
+# get a random data
+def get_random_data():
+	data = unlabeled_dataset.sample().values.tolist()
+	return data[0]
+
+def convert_str(data):
+	return Markup(data[3].replace('<MEDICINE>', '<span class="medicine">'+data[1]+'</span>').replace('<EFFECT>', '<span class="effect">'+data[2]+'</span>'))
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,7 +1,36 @@
+
 function relationCheck() {
     if (document.getElementById('smCheck').checked) {
         document.getElementById('smOutcome').style.display = 'block';
-    } else {
-        document.getElementById('smOutcome').style.display = 'none';
+        document.getElementById('msOutcome').style.display = 'none';
+        document.getElementById('mwOutcome').style.display = 'none';
+        document.getElementById('case').style.display = 'block';
     }
+	else if (document.getElementById('msCheck').checked) {
+        document.getElementById('smOutcome').style.display = 'none';
+        document.getElementById('msOutcome').style.display = 'block';
+        document.getElementById('mwOutcome').style.display = 'none';
+        document.getElementById('case').style.display = 'block';
+    }
+    else if (document.getElementById('mwCheck').checked) {
+        document.getElementById('smOutcome').style.display = 'none';
+        document.getElementById('msOutcome').style.display = 'none';
+        document.getElementById('mwOutcome').style.display = 'block';
+        document.getElementById('case').style.display = 'block';
+    }
+    else {
+    	document.getElementById('smOutcome').style.display = 'none';
+        document.getElementById('msOutcome').style.display = 'none';
+        document.getElementById('mwOutcome').style.display = 'none';
+        document.getElementById('case').style.display = 'none';
+    }
+}
+
+function getData() {
+	xmlhttp= new XMLHttpRequet();
+	
+}
+
+function sendLabelAndNext() {
+
 }
