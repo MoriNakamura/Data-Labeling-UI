c4f56310f495ba250786a99ae6c9ab084605a6e3
Frank Chung <MoriNakamura@users.noreply.github.com>
2019-03-14 18:09:43 +0800

    Added jQuery, TODO: form submit bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,10 @@
-from flask import Flask, render_template, request, Markup
+from flask import Flask, render_template, request, Markup, jsonify
 
 # For dataset reading and writing
 import pandas as pd
 
 unlabeled_dataset = pd.read_csv('./dataset/all_unlabeled_data.tsv', sep='\t')
 
-
-# labeled_dataset = 
-
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/index', methods=['GET', 'POST'])
-def main():
-	if request.method == 'GET':
-		data = get_random_data()
-		print(data)
-		return render_template('index.html', _id=str(data[0]), text=convert_str(data))
-	else:
-		#TODO receive data
-		return ender_template('index.html')
 
 # get a random data
 def get_random_data():
@@ -28,3 +13,33 @@ def get_random_data():
 
 def convert_str(data):
 	return Markup(data[3].replace('<MEDICINE>', '<span class="medicine">'+data[1]+'</span>').replace('<EFFECT>', '<span class="effect">'+data[2]+'</span>'))
+
+
+# labeled_dataset = 
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index', methods=['GET'])
+def index():
+	
+	return render_template('index.html')
+
+@app.route('/getdata', methods=['GET'])
+def handle_getdata():
+	data = get_random_data()
+	_id=str(data[0])
+	text=convert_str(data)
+	d = {'_id': _id, 'text': text}
+	return jsonify(d)
+
+@app.route('/submit', methods=['POST'])
+def handle_submit():
+	print('---------------------')
+	print(request.form)
+	print('---------------------')
+	data = get_random_data()
+	_id=str(data[0])
+	text=convert_str(data)
+	d = {'_id': _id, 'text': text}
+	return jsonify(d)
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -26,11 +26,79 @@ function relationCheck() {
     }
 }
 
-function getData() {
-	xmlhttp= new XMLHttpRequet();
+$(document).ready(function(){
+	$.get('getdata', function(data) {
+		$('#labeling_id').text('Text '+data._id);
+		$('#labeling_text').html(data.text);
+		$('#submit_id').val(data._id);
+	});
+});
+
+$('#labelForm').submit(function(event) {
+	alert('lalala')
+	event.preventDefault();
+	var data = $(this).serialize();
 	
-}
 
-function sendLabelAndNext() {
+	$.post('/submit', data, function(response) {
+		$('#labeling_id').text('Text '+response._id);
+		$('#labeling_text').html(response.text);
+		$('#submit_id').val(response._id);
+	});
+});
 
-}
+// window.addEventListener("load", function() {
+// 	var XHR = new XMLHttpRequest();
+
+// 	XHR.onreadystatechange = function() {
+//     	if (XHR.readyState == XMLHttpRequest.DONE) {
+//     		if(xhr.getResponseHeader('content-type')==='application/json'){
+// 	        	var result = JSON.parse(xhr.responseText);
+// 	        	console.log(result);
+// 	        }
+//     	}
+//     }
+
+// 	XHR.open("GET", "http://192.168.2.108:5000/getdata");
+// 	XHR.send(null);
+// });
+
+// function sendLabelAndNext() {
+// 	var xhr = new XMLHttpRequest();
+
+// }
+
+// document.getElementById('labelForm').addEventListener('submit', function(evt) {
+// 	evt.preventDefault();
+//     var XHR = new XMLHttpRequest();
+
+//     XHR.onreadystatechange = function() {
+//     	if (XHR.readyState == XMLHttpRequest.DONE) {
+//     		if(xhr.getResponseHeader('content-type')==='application/json'){
+// 	        	var result = JSON.parse(xhr.responseText);
+// 	        	console.log(result);
+//     	}
+//     }
+
+//     // Bind the FormData object and the form element
+//     var FD = new FormData(form);
+
+//     // Define what happens on successful data submission
+//     XHR.addEventListener("load", function(event) {
+//       alert(event.target.responseText);
+//     });
+
+//     // Define what happens in case of error
+//     XHR.addEventListener("error", function(event) {
+//       alert('Oops! Something went wrong.');
+//     });
+
+//     // Set up our request
+//     XHR.open("POST", "http://192.168.2.108:5000/submit");
+
+//     // The data sent is what the user provided in the form
+//     XHR.send(FD);
+//   }
+ 
+//   // Access the form element...
+// });
